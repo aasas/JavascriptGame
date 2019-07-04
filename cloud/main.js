7ce5d87ac3d7c78bf188a93cbcf0123b5220eff7
aasas <mouhamadoumoustaphaba521@gmail.com>
2019-07-04 08:09:47 +0200

    first commit

---
+++ cloud/main.js
@@ -0,0 +1,51 @@
+// Parse.Cloud.beforeSave("Score", (request) => {
+//   if (request.object.get("score") % 2 == 0) {
+//     request.object.set("isEvenNumber", true);
+//   } else{
+//     request.object.set("isEvenNumber", false);
+//   }
+//   console.log(request.object.get("score"));
+// });
+
+
+Parse.Cloud.define("getUser", async (request) => {
+  var query = new Parse.Query("User");
+  query.equalTo("objectId", request.params.user)
+  .find()
+  .then((results) => {
+    console.log("results: "+ results.length);
+    Response.success(results[0].get("username"));
+  })
+  .catch(() => {
+    Response.error("Score failed")
+  });
+});
+
+
+Parse.Cloud.job("myJob", (request) =>  {
+  var query = new Parse.Query("Score");
+
+  query.find({
+    success: function(results){
+      console.log("successfully retrieved " + results.length + "scores.");
+
+      query.each(function(object, err){
+        object.destroy({
+          success: function(object){
+            console.log("successfully destroyed object, ");
+            Response.success();
+          },
+          error: function(error){
+            console.log("error: "+ error.code +"="+ error.message);
+            Response.error("Score lookup failed");
+          }
+        })
+      })
+    },
+    error: function(error){
+      console.log("error: "+ error.code +"="+ error.message);
+      Response.error("Score lookup failed");
+    }
+    
+  });
+});
---
+++ public/js/parse.js
@@ -0,0 +1,7 @@
+// PART 1: Put your APP ID, JS Key, and SERVER URL
+Parse.initialize(
+  'dVEefsPv90Y9Ievq9Xhl8tsfKXoxtf0SUfegj6Oz', // YOUR APP ID
+  'q024iTINbB1YW7nZSpC3osk6imQ5qLqKWgLtniXD' // YOUR Javascript  KEY
+);
+// YOUR SERVER URL
+Parse.serverURL = 'https://parseapi.back4app.com/';
